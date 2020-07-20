570d3fb962ca92116d927c7b6e9903fa45fa3b0a
Botmonitor <36127687+Botmonitor@users.noreply.github.com>
2020-07-20 14:20:26 +0700

    Create config-example.js

---
+++ config-example.js
@@ -0,0 +1,26 @@
+module.exports = {
+  // bot settings
+  bot_token: '730356324:AAHGlG7XlYRKRjaueTefpKIw_a3mix4w0ro', // support bot token
+  staffchat_id: '-1001194826358', // eg. -123456789
+  owner_id: '361572907',
+  adminChatId: 361572907, 
+  spam_time: 5 * 60 * 1000, // time (in MS) in which user may send 5 messages
+
+  // customize your language
+  startCommandText: 'Welcome in our support chat! Ask your question here.',
+  faqCommandText: 'Check out our FAQ here: <a href="http://linktoyourfaq">linktoyourfaq.com</a>',
+  lang_contactMessage:
+    `Thank you for contacting us. We will answer as soon as possible.`,
+  lang_blockedSpam:
+    `You sent quite a number of questions in the last while.
+    Please calm down and wait until staff reviews them.`,
+  lang_ticket: 'Ticket',
+  lang_acceptedBy: 'was accepted by',
+  lang_dear: 'Dear',
+  lang_regards: 'Best regards,',
+  lang_from: 'from',
+  lang_language: 'Language',
+  lang_msg_sent: 'Message sent to user',
+  lang_usr_with_ticket: 'User with ticket',
+  lang_banned: 'banned',
+};
