6d27978e631d3adb942d6bbcce33033f72b8cdaa
Botmonitor <36127687+Botmonitor@users.noreply.github.com>
2020-07-21 09:56:37 +0700

    BMonitoringSupport

--- config-example.js
+++ config-example.js
@@ -1,9 +1,11 @@
 module.exports = {
   // bot settings
-  bot_token: '730356324:AAHGlG7XlYRKRjaueTefpKIw_a3mix4w0ro', // support bot token
+  bot_token: '539235355:AAHgwXE2gI2dBuF0GImu0sVuFEZGQFY7FCQ', // support bot token
   staffchat_id: '-1001194826358', // eg. -123456789
   owner_id: '361572907',
-  adminChatId: 361572907, 
+  owner_id: '601954832',
+  adminChatId: 361572907,
+  adminChatId: 601954832,
   spam_time: 5 * 60 * 1000, // time (in MS) in which user may send 5 messages
 
   // customize your language
